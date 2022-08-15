fe07ac4b5eb30d5637fadfd1a3b06389668c502b
Samuel Braun <office.samigo.a@gmail.com>
2022-08-15 13:33:32 +0200

    Added metadata to themes

--- themes/themes.js
+++ themes/themes.js
@@ -2,7 +2,19 @@
 import DefaultUI from './Default/Default.js';
 import Moonlit from './Moonlit/Moonlit.js';
 
-export const Themes = {
-    Default: DefaultUI,
-    Moonlit: Moonlit,
-};
+export const Themes = [
+    {
+        name: "Default",
+        theme: DefaultUI,
+        authors: ["MindLaborDev"],
+        preview: ".png",
+        description: "The default theme.",
+    },
+    {
+        name: "Moonlit",
+        theme: Moonlit,
+        authors: ["MindLaborDev"],
+        preview: ".png",
+        description: "The theme with a dark background.",
+    }
+];
